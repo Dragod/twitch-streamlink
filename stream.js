3f1f8c5cef46df89362a0b9909a134bae14ef516
Fabio <fabio@screendragon.com>
2022-05-28 18:54:14 +0200

    update

--- stream.js
+++ stream.js
@@ -3,6 +3,7 @@ const path = require('path')
 const jsonPath = './config.json'
 const filePath = path.resolve(__dirname, jsonPath)
 
+//Create a new instance of Stream class
 let stream = new Stream(filePath)
 
 stream.start()
